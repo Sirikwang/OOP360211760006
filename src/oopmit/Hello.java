c13f7bea44274d1cb5470970b550937acdcf9c7e
Siriwan <Narak_gvang@hotmail.com>
2018-05-08 15:13:54 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -3,6 +3,8 @@ package oopmit;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, My name is Siriwan");
-
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }//main
 }//class
