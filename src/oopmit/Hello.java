936c04e6b0be5b4538a88594203086fb6b7e05a6
Siriwan <Narak_gvang@hotmail.com>
2018-05-08 15:20:47 +0700

    delete data1

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, My name is Siriwan");
         System.out.println("Major: Information System");
-        System.out.println("Faculty: Management Technology");
+
 
     }//main
 }//class
